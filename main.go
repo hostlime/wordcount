417c81a509f17f6f6644e3ae0783d3598a2fc803
hostlime <hostlime@ya.ru>
2022-07-31 15:04:39 +0300

    fix 0

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
+	if (len(os.Args) > 1) && (len(os.Args[1]) > 0) {
 		stringWordBuff := strings.Split(os.Args[1], " ")
 		fmt.Println(len(stringWordBuff))
+	} else {
+		fmt.Println(0)
 	}
 }
